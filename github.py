0805d1670b037d1014a07502ee45b40049ee4cf0
minhtran317 <minh.tran@ufl.edu>
2024-04-11 15:11:29 -0400

    decode function created and encode function cleaned

--- github.py
+++ github.py
@@ -2,9 +2,17 @@
 def encoder(num_list):
     encoded_password = ""
     for i in num_list:
-        encoded_digit = str((int(i) + 3) % 10)
-        encoded_password += encoded_digit
+        encoded_password += str((int(i) + 3) % 10)
     return encoded_password
 
+
+def decode(encoded_password: str):
+    decoded_password = ""
+    for num in encoded_password:
+        decoded_password += str((int(num) - 3) % 10)
+    return decoded_password
+
+
 print(encoder("00009962"))
+print(decode("33332295"))
 
