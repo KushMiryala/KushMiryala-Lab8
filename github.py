4ecc44f50800787ddfce225c04a6b0a72c0749ae
KushMiryala <kushmiryala09@gmail.com>
2024-04-11 15:13:12 -0400

    Added menu

--- github.py
+++ github.py
@@ -2,9 +2,30 @@
 def encoder(num_list):
     encoded_password = ""
     for i in num_list:
-        encoded_digit = str((int(i) + 3) % 10)
-        encoded_password += encoded_digit
+        encoded_password += str((int(i) + 3) % 10)
     return encoded_password
 
-print(encoder("00009962"))
+def decode_password(encoded_password):
+    decoded_password = ""
+    for digit in encoded_password:
+        decoded_digit = str((int(digit) - 3) % 10)
+        decoded_password += decoded_digit
+    return decoded_password
 
+
+def main():
+    encoded_password = ""
+    while True:
+        menu_selection = input(str("Please enter an option: "))
+        if menu_selection == '1':
+            password = input(str("Please enter your password to encode: "))
+            print("Your password has been encoded and stored!")
+            encoded_password = encoder(password)
+        elif menu_selection == '2':
+            original_password = decode_password(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {original_password}")
+        elif menu_selection == '3':
+            break
+
+if __name__ ==  "__main__":
+    main()
